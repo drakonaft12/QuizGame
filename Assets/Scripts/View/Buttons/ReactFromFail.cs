c0be8fad83ffd153f2eed80a805f76a47610867f
drakonaft12 <61424327+drakonaft12@users.noreply.github.com>
2024-07-29 17:26:47 +0300

    add DOKill

--- Assets/Scripts/View/Buttons/ReactFromFail.cs
+++ Assets/Scripts/View/Buttons/ReactFromFail.cs
@@ -1,10 +1,5 @@
 using DG.Tweening;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
-using UnityEngine.UI;
-using View;
 
 namespace View
 {
@@ -14,7 +9,6 @@ namespace View
         [SerializeField] private float amplitude = 1;
         [SerializeField] private float durationEffect = 0.9f;
         [SerializeField] private float durationPause = 0.3f;
-        private Tween currentTween;
         private Vector3 clickPosition;
 
         private void Awake()
@@ -29,10 +23,10 @@ namespace View
                 return;
             }
             
-            currentTween?.Kill();
-            currentTween = DOTween.Sequence()
-                           .AppendInterval(durationPause)
-                           .Append(transform.DOPunchPosition(clickPosition, durationEffect, 10, 3));
+            transform.DOKill();
+            DOTween.Sequence()
+            .AppendInterval(durationPause)
+            .Append(transform.DOPunchPosition(clickPosition, durationEffect, 10, 3));
         }
 
     }
